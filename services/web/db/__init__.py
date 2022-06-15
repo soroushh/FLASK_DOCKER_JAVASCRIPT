955c166a1b8099c85a8e4a8846029d0ed1cd44a5
soroushh <sorush.kh68@gmail.com>
2022-06-15 10:20:26 +0100

    The files are structured.

---
+++ services/web/db/__init__.py
@@ -0,0 +1,3 @@
+"""Includes the db definition."""
+from flask_sqlalchemy import SQLAlchemy
+db = SQLAlchemy()
--- services/web/project/__init__.py
+++ services/web/project/__init__.py
@@ -1,24 +1,22 @@
 """The file including the flask app."""
 from flask import Flask, jsonify
-from flask_sqlalchemy import SQLAlchemy
+from db import db
+from models.user import User
 
 
 app = Flask(__name__)
 app.config.from_object("project.config.Config")
-db = SQLAlchemy(app)
-
-
-class User(db.Model):
-    __tablename__ = "users"
-
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(128), unique=True, nullable=False)
-    active = db.Column(db.Boolean(), default=True, nullable=False)
-
-    def __init__(self, email):
-        self.email = email
+db.init_app(app)
 
 
 @app.route("/")
 def hello_world():
     return jsonify(hello="world")
+
+@app.route('/user/add/<email>')
+def add_user(email):
+    """Adds a user to db."""
+    db.session.add(User(email=email))
+    db.session.commit()
+
+    return jsonify({'message': 'User is added.'})
