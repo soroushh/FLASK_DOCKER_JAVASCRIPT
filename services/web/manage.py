6f75c7edc487d7e21a8baae46bd38c54718f3c0f
soroushh <sorush.kh68@gmail.com>
2022-06-15 12:55:01 +0100

    The seed and creation of db is done through the sqlalchemy.

--- services/web/manage.py
+++ services/web/manage.py
@@ -1,24 +1,27 @@
 """The file for handling the cli commands."""
 from flask.cli import FlaskGroup
+from sqlalchemy.orm import Session
+import os
 
-from project import app
+
+from project import create_db_engine, User, app
+from models.user import meta
 
 
 cli = FlaskGroup(app)
 
 @cli.command("create_db")
 def create_db():
-    # db.drop_all()
-    # db.create_all()
-    # db.session.commit()
-    pass
+    engine = create_db_engine(os.getenv('DATABASE_URL'))
+    meta.drop_all(engine)
+    meta.create_all(engine)
 
 
 @cli.command("seed_db")
 def seed_db():
-    # db.session.add(User(email="michael@mherman.org"))
-    # db.session.commit()
-    pass
+    with Session(create_db_engine(db_url=os.getenv('DATABASE_URL'))) as session:
+        session.add(User(email='a@a.com', active=True))
+        session.commit()
 
 
 if __name__ == "__main__":
