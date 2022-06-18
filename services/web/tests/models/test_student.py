37ec28f01bd9868b7b377dc569084f08ba870e9a
soroushh <sorush.kh68@gmail.com>
2022-06-18 17:48:02 +0100

    The tests related to adding an student is added.

--- services/web/tests/models/test_student.py
+++ services/web/tests/models/test_student.py
@@ -1,5 +1,8 @@
 """Tests related to Student model."""
 from models.student import Student
+from db import create_db_engine, meta
+from mappers import User, Address, Student
+from sqlalchemy.orm import Session
 
 def test_model():
     """."""
@@ -14,3 +17,52 @@ def test_model():
     assert isinstance(student, Student)
 
     assert student.number == 15
+
+
+def test_adding_student_adds_user_and_student():
+    """."""
+    engine = create_db_engine(db_url='sqlite:///')
+    meta.drop_all(engine)
+    meta.create_all(engine)
+    with Session(engine) as session:
+        users = session.query(User).all()
+        students = session.query(Student).all()
+        addresses = session.query(Address).all()
+
+        assert len(students) == 0
+        assert len(users) == 0
+        assert len(addresses) == 0
+
+        session.add(
+            Student(
+                email='a@a.com',
+                active=True,
+                job='student',
+                number=10,
+                addresses=[
+                    Address(
+                        email_address='b@b.com'
+                    )
+                ]
+            )
+        )
+        session.commit()
+
+        users = session.query(User).all()
+        students = session.query(Student).all()
+        addresses = session.query(Address).all()
+
+    assert len(users) == 1
+
+    assert users[0].id == 1
+    assert users[0].email == 'a@a.com'
+    assert users[0].job == 'student'
+
+    assert len(students) == 1
+    assert students[0].id == 1
+    assert students[0].number == 10
+
+    assert len(addresses) == 1
+    assert addresses[0].id == 1
+    assert addresses[0].email_address == 'b@b.com'
+    assert addresses[0].user_id == 1
